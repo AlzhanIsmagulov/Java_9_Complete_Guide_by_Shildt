232d98daa640338592dfe8238af0923d95c6fc1a
alzhan <aismagulov@gmail.com>
2019-10-26 11:47:13 +0200

    Example 2

---
+++ src/Chapter_2/Example2.java
@@ -0,0 +1,16 @@
+package Chapter_2;
+
+/**
+ * Это ещё один пример короткой программы.
+ * Присвоить исходному файлу имя "Example2.java"
+ */
+class Example2 {
+    public static void main(String[] args) {
+        int num; //В этой строке объявляется переменная с именем num
+        num = 100; //В этой строке кода переменной num присваивается значение 100
+        System.out.println("Это переменная num: " + num);
+        num = num * 2;
+        System.out.println("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
