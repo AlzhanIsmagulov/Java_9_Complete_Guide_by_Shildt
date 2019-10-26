8b24fa795cbed0699f71453d565f56c767da132f
alzhan <aismagulov@gmail.com>
2019-10-26 11:36:52 +0200

    Initial commit

---
+++ src/Chapter_2/Example.java
@@ -0,0 +1,12 @@
+package Chapter_2;
+
+/**
+* Это простая программа на Java.
+* Присвоить исходному файлу имя "Example Java"
+*/
+class Example {
+    //Эта программа начинается с вызова метода Main()
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+}
